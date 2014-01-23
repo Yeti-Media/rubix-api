d81677039aa191b35ad051b90ef8ec905e13ee8f
Nicolas Santa <nicolas55ar@gmail.com>
2014-01-22 22:54:17 -0300

    add upload remote url for pattern

--- app/controllers/api/v1/patterns_controller.rb
+++ app/controllers/api/v1/patterns_controller.rb
@@ -13,6 +13,7 @@ class Api::V1::PatternsController < Api::V1::BaseController
   api :GET, "/api/v1/patterns", "Add a new pattern to the training set"
   param :pattern, Hash do
     param :file, File, desc: "Image file", required: true
+    param :remote_file_url, String, desc: "Image url", required: true
     param :label, String, desc: "Label (used for identification). If the image has the category 'ocr' Then the label should contain the text in the image", required: true
     param :category_name, String, desc: "Category: (matching, comparison, ocr or face)", required: true
     param :category_id, Integer , desc: "Category ID: retrieved from /api/v1/categories endpoint. Can be provided if category_name is not present", required: true
@@ -21,7 +22,7 @@ class Api::V1::PatternsController < Api::V1::BaseController
  
 
   def create
-    @pattern = @user.patterns.new(params[:pattern].permit(:label, :file, :category_name, :category_id))
+    @pattern = @user.patterns.new(params[:pattern].permit(:label, :file, :remote_file_url, :category_name, :category_id))
     if @pattern.save
       render json: @pattern.to_json(only: [:id,:label, :file, :category_id, :category_name]) , status: :ok
     else
