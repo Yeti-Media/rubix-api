9d296f646e796f8869d747f2127a72f008fca409
Nicolas Santa <nicolas55ar@gmail.com>
2014-01-24 22:48:48 -0300

    skip CRSF v2

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::BaseController < ApplicationController
   before_filter :cors_set_access_control_headers
   before_filter :authenticate_with_token!
-  skip_before_filter :verify_authenticity_token
+  protect_from_forgery with: :null_session
 
   protected
 
