8859bee13f265f69bac8d597159e5fd63db69cc1
Nicolas Santa <nicolas55ar@gmail.com>
2014-04-14 19:35:50 -0300

    add not authorized helper

--- app/lib/controllers/authentication.rb
+++ app/lib/controllers/authentication.rb
@@ -7,13 +7,14 @@ module Controllers
         authenticate_with_3scale!
       elsif params[:access_token]
         authenticate_with_token!
+      else
+        not_authorized!
       end
     end
 
     def authenticate_with_token!
       unless load_user(params[:access_token])
-        render json: {error: 'Not Authorized'}, status: 401
-        false
+        not_authorized!
       else
         true
       end
@@ -25,13 +26,17 @@ module Controllers
       if response.success? && load_user(request.headers['HTTP_USER_KEY'])
         true
       else
-        render json: {error: "Not Authorized"}, status: 401 
-        false
+        not_authorized!
       end
     end
 
     def load_user(token)
       @user = User.find_by(access_token: token)
+    end
+
+    def not_authorized!
+      render json: {error: 'Not Authorized'}, status: 401
+      false
     end
 
   end
