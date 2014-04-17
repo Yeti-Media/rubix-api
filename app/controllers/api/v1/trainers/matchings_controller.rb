d47a482dfbe7afb87e3f3521e02f47476eb66acf
Nicolas Santa <nicolas55ar@gmail.com>
2014-04-17 16:25:59 -0300

    fix typo

--- app/controllers/api/v1/trainers/matchings_controller.rb
+++ app/controllers/api/v1/trainers/matchings_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::Trainers::MatchingsController < Api::V1::BaseController
 
   def show
-    trainer = Anakin::Trainer.new(current_user)
-    trailer.train!
+    trainer = Anakin::Trainer.new(@user)
+    trainer.train!
     render nothing: true, status: :ok
   end
 end
